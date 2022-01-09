fe31bce847e1cf529b97182dd5eddaf75ebfad45
nobertof <francisco.noberto.gomes@gmail.com>
2022-01-09 12:29:42 -0300

    cirando atendimentos 👌

---
+++ controllers/atendimentos.js
@@ -0,0 +1,12 @@
+const Atendimento = require('../models/atendimentos')
+module.exports = (app) => {
+  app.get("/atendimentos", (req, res, next) =>
+    res.send("Você está na rota de atendimentos e está realizando um get")
+  );
+  app.post("/atendimentos", (req, res, next) => {
+    const atendimento = req.body
+
+    Atendimento.adiciona(atendimento)
+    res.send('Post Atendimento')
+  });
+};
