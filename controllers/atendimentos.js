c75377d130be24dd2e45c1fb6c31a480c36df3e5
nobertof <francisco.noberto.gomes@gmail.com>
2022-01-09 21:34:25 -0300

    atendimentos 👌

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,6 +1,6 @@
 const Atendimento = require('../models/atendimentos')
 module.exports = (app) => {
-  app.get("/atendimentos", (req, res, next) =>
+  app.get("/atendimentos", (req., res, next) =>
     res.send("Você está na rota de atendimentos e está realizando um get")
   );
   app.post("/atendimentos", (req, res, next) => {
